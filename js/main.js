d62fb0d4a35b3abef8ef12080aa30c9b6fd691c2
SoyRandy708 <randy708ochoa@gmail.com>
2022-10-10 18:18:00 -0500

    Ahora el carrito de compras aparece

--- js/main.js
+++ js/main.js
@@ -2,13 +2,21 @@ const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+const carritoIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 
 
 menuEmail.addEventListener("click", toggleMenus)
 menuHamIcon.addEventListener("click", toggleMenus)
-
+carritoIcon.addEventListener("click", toggleAsideCarrito)
 
 function toggleMenus() {
+    aside.classList.add("inactive")
     desktopMenu.classList.toggle("inactive")
     mobileMenu.classList.toggle("inactive")
+}
+function toggleAsideCarrito() {
+    mobileMenu.classList.add("inactive")
+    desktopMenu.classList.add("inactive")
+    aside.classList.toggle("inactive")
 }
